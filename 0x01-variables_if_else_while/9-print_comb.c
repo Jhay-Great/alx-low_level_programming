0e9a174cdc99613c8ba2727c4cf47d470797f047
Jhay-Great <alemonai.jenef@gmail.com>
2022-10-17 15:44:28 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - Entry
+  * Return: 0
+  */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar (x + '0');
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
