553ae87ed69433cbabd99a05d4c728ef715c852c
Jhay-Great <alemonai.jenef@gmail.com>
2022-10-17 22:18:50 -0700

    advance task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: 0
+  */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+			}
+			else if (a == b)
+			{
+				continue;
+			}
+			else if (a == '8' && b == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
