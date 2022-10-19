d024fa8943e9fffdba10f170710cfd8b34d48670
Jhay-Great <alemonai.jenef@gmail.com>
2022-10-19 15:57:59 -0700

    task 1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: printing alphabets in lowercae
+ * Return: Always 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+	return (0);
+}
