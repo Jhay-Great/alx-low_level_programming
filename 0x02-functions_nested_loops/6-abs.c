43839cdac9f6fbeb3ec8c648b241eec9df904408
Jhay-Great <alemonai.jenef@gmail.com>
2022-10-19 20:41:50 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * _abs - function prototype
+  * @x: parameter
+  * Description: computing absolute value
+  * Return: 0
+  */
+int _abs(int x)
+{
+	x = x * x;
+	_putchar(x);
+	return (x);
+}
