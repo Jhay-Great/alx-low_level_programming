3dbb1f52a598d619de59a93403d3057d781a188c
Jhay-Great <alemonai.jenef@gmail.com>
2022-10-20 23:01:45 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * _isupper - function prototype
+  * @c: parameter
+  * Description: uppercase character checker
+  * Return: 0
+  */
+int _isdigit(int c)
+{
+	if (c >= 0  && c <= 9)
+		return (1);
+	else
+		return (0);
+}
