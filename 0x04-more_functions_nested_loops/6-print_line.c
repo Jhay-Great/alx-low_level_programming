573e0aa7235c537e46ee657e9c227c6f38420588
Jhay-Great <alemonai.jenef@gmail.com>
2022-10-22 08:30:27 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * print_line - prototype function
+  * @n: parameter
+  * main - printing a straight line
+  * Return: Always 0.
+  */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('_');
+	}
+}
+
