ae60d0f7a1b771b6ac95ba368069f46f0f3ddff3
Jhay-Great <alemonai.jenef@gmail.com>
2022-10-25 11:57:39 -0700

    another correction for task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98: function prototype used to update the value of n to 98
- * @n:input
+ * reset_to_98 - function prototype used to update the value of n to 98
+ * @n: input
  * Return: n
  */
 void reset_to_98(int *n)
