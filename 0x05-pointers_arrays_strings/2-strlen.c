39f2ba38165a50f7ef000e2d904f4c24deecd080
Jhay-Great <alemonai.jenef@gmail.com>
2022-11-06 15:59:00 +0000

    task 2 completed

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _strlen - function prototype
+  * @s: is a parameter
+  * Description: function to count the length of a string
+  * Return: count.
+  */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+
+	}
+	return (count);
+}
