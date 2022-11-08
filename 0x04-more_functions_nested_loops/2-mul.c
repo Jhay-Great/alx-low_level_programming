772f588552830b826272f511fe4ecb2d3d23a250
Jhay-Great <alemonai.jenef@gmail.com>
2022-11-08 21:13:47 +0000

    task 2 completed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * mul - function prototype
+  * @a: int parameter
+  * @b: int parameter
+  * description: a function that multiples two integers
+  * Return: 0
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
