eda46cbe4ed53e2ccd409e1b2f254afd01bbcd07
Jhay-Great <alemonai.jenef@gmail.com>
2022-11-12 09:32:03 +0000

    task 10: 10-add.c done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * add - function prototype
+  * @a: parameter
+  * @b: parameter
+  * Description: addition of two integers
+  * Return: a + b;
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
