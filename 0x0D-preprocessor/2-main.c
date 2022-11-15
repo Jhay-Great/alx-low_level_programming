3b9f440176ae94c5eded5dbdb096e087c26161e2
Jhay-Great <alemonai.jenef@gmail.com>
2022-11-15 08:40:20 +0000

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - entry point of our programm
+  * @argc: integer argument parameter
+  * @argv: character argument parameter
+  * Return: Always 0.
+  */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+	}	
+	return (0);
+}
