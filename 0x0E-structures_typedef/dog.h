5c1a15b6808498d3902238d94b7725d293f0f2f4
Jhay-Great <alemonai.jenef@gmail.com>
2022-11-18 06:52:14 +0000

    0. poppy done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,17 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - short description
+  * @name: first member
+  * @age: second member
+  * @owner: third member
+  * Descriptioin: this is a structure that accpets 2 char and 1 int
+  */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
